db7ff086ad13b6f7aca8055c6f447bb5eec0dbd0
Mahish <mahishmistry@umass.edu>
2026-09-11 09:26:30 -0400

    initial Express server with two routes

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+const projects = [
+  { name: 'Weather app', tag: 'javascript' },
+  { name: 'Portfolio site', tag: 'express' },
+  { name: 'Budget tracker', tag: 'python' },
+];
+
+app.get('/', (req, res) => {
+  res.send('Hello, web!');
+});
+app.get('/projects', (req, res) => {
+  const tag = req.query.tag;
+  const name = req.query.name
+  // filter `projects` here, based on your decision above
+  const filterprojects = tag ? projects.filter(projects => projects.tag === tag.toLowerCase()):projects;
+  res.json(filterprojects)
+  const sort = name ? projects.sort(projects => projects.tag === tag.toLowerCase()):projects;
+  res.json(sort)
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
